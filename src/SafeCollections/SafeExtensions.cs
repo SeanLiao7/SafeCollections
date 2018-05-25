79083f063738134da86257cb572bd09cb46d93a1
Sean Liao <sean@pixis.com.tw>
2018-05-25 14:47:06 +0800

    1. 新應擴充方法。

--- src/SafeCollections/SafeExtensions.cs
+++ src/SafeCollections/SafeExtensions.cs
@@ -7,7 +7,13 @@ namespace SafeCollections
         public static IEnumerable<T> AsLocked<T>(this IEnumerable<T> enumerable, object locker)
             => new SafeEnumerable<T>(enumerable, locker);
 
+        public static SafeCollection<T> ToSafeCollection<T>(this ICollection<T> collection, object locker = null)
+            => new SafeCollection<T>(collection, locker);
+
         public static SafeList<T> ToSafeList<T>(this List<T> list, object locker = null)
             => new SafeList<T>(list, locker);
+
+        public static SafeDictionary<TKey, TValue> ToSafeDictionary<TKey, TValue>(this IDictionary<TKey, TValue> table, object locker = null)
+            => new SafeDictionary<TKey, TValue>(table, locker);
     }
 }
