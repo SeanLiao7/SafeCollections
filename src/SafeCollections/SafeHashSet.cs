e448e1f6a117f83b4fb90b49a81dad4ba0ee4c13
Sean Liao <sean@pixis.com.tw>
2018-07-18 10:14:26 +0800

    1. SafeHashSet 支援遞迴進入鎖。

--- src/SafeCollections/SafeHashSet.cs
+++ src/SafeCollections/SafeHashSet.cs
@@ -13,7 +13,7 @@ namespace SafeCollections
         public SafeHashSet(ISet<T> hashSet = null, ReaderWriterLockTiny locker = null)
         {
             _hashSet = hashSet ?? new HashSet<T>();
-            _locker = locker ?? new ReaderWriterLockTiny();
+            _locker = locker ?? new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
         }
 
         void ICollection<T>.Add(T item)
